64a01da8ec4c364580e036c1e94f40421f1324f1
Anubisa97 <assamsias97@gmail.com>
2023-09-30 19:31:09 +0300

    finished exercise

--- 4-exercises-DomIntroduction/6-formatTheText/solution.js
+++ 4-exercises-DomIntroduction/6-formatTheText/solution.js
@@ -5,28 +5,25 @@ function solve() {
   let sentences = [];
 
   for (const el of inputConverted) {
-    if (el.length > 0) {
+    if (el.length > 1) {
       sentences.push(el);
-      console.log(el);
     }
   }
 
   let output = [];
   while (sentences.length >= 1) {
-    if (output.length > 2) {
-      outputEl.innerHTML += `<p> ${output.join(".")} </p>`;
+    if (sentences.length > 2) {
+      output.push(sentences.splice(0, 3));
+      outputEl.innerHTML += `<p> ${output.join(". ")} </p>`;
       output = [];
-    } else if (sentences.length === 1) {
-      if (output.length === 0) {
-        output.push(sentences.shift());
-      }
-      outputEl.innerHTML += `<p> ${output.join(".")} </p>`;
+    } else if (sentences.length > 1) {
+      output.push(sentences.splice(0, 2));
+      outputEl.innerHTML += `<p> ${output.join(". ")} </p>`;
       output = [];
-    } else if (sentences.length === 2 && output.length < 2) {
-      outputEl.innerHTML += `<p> ${output.join(".")} </p>`;
+    } else if (sentences.length > 0) {
+      output.push(sentences.splice(0, 1));
+      outputEl.innerHTML += `<p> ${output.join(". ")} </p>`;
       output = [];
-    } else {
-      output.push(sentences.shift());
     }
   }
 }
